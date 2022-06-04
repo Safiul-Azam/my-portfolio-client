ca645e562c5cc28afc8592b2f9b46653fbf80fdc
mdsafiulazam <mdriadkhan20@gmail.com>
2022-06-04 10:26:41 +0600

    used tailwind css,dasiyui and react router

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        myPortfolio: {
+          primary: "#ff5200",
+          secondary: "#ffffff12",
+          accent: "#111111",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
