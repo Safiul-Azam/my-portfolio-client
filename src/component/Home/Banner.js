418107a1cd4f66d3f51071b140cc553b43403426
Safiul-Azam <safiulazamriad@gmail.com>
2022-09-29 15:56:33 +0600

    change size

--- src/component/Home/Banner.js
+++ src/component/Home/Banner.js
@@ -11,11 +11,9 @@ const Banner = () => {
     return (
         <div className='bg-accent h-auto pt-16' id='home'>
             <div class="container mx-auto block md:flex lg:flex items-center flex-row">
-                <div className='lg:w-1/2 mt-5 w-full px-8 pb-10 mx-auto rounded-xl'>
                     <img className='rounded border-l-8 border-b-8 border-primary shadow-2xl w-full lg:w-3/4' src={myPhoto} alt='MyPhoto' />
-                </div>
                 <div
-                    className='lg:w-3/4 w-full px-8'>
+                    className='lg:w-1/2 w-full px-8'>
                     <h4 className='lg:text-xl text-lg text-white mb-4'>Hello!</h4>
                     <h2 class="lg:text-5xl text-3xl text-primary font-bold mb-4">This is Safiul Azam</h2>
                     <h3 class="lg:text-3xl text-2xl font-bold text-white mb-6 tracking-widest">
