c6fb6b5b0e104ec3ecf87068763f81460f62b156
Safiul-Azam <safiulazamriad@gmail.com>
2022-10-02 09:22:55 +0600

    install yarn and change in banner.js

--- src/component/Home/Banner.js
+++ src/component/Home/Banner.js
@@ -9,19 +9,18 @@ import Typewriter from 'typewriter-effect';
 const Banner = () => {
 
     return (
-        <div className='bg-accent h-auto pt-16' id='home'>
-            <div class="container mx-auto block md:flex lg:flex items-center flex-row">
-                    <img className='rounded border-l-8 border-b-8 border-primary shadow-2xl w-full lg:w-3/4' src={myPhoto} alt='MyPhoto' />
-                <div
-                    className='lg:w-1/2 w-full px-8'>
+        <div className='bg-accent pt-24 pb-10' id='home'>
+            <div class="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 items-center w-3/4 mx-auto">
+                    <img className='rounded border-b-8  border-l-8 border-primary bg-black shadow-2xl w-full md:w-1/2 lg:w-1/2' src={myPhoto} alt='MyPhoto' />
+                <div className='lg:w-full w-full lg:px-8'>
                     <h4 className='lg:text-xl text-lg text-white mb-4'>Hello!</h4>
                     <h2 class="lg:text-5xl text-3xl text-primary font-bold mb-4">This is Safiul Azam</h2>
-                    <h3 class="lg:text-3xl text-2xl font-bold text-white mb-6 tracking-widest">
+                    <h3 class="lg:text-3xl text-xl font-bold text-white mb-6 tracking-widest">
                         <Typewriter
                             options={{
                                 strings: [
                                  'Front-End  Developer',
-                                 'Full-stack web Developer',
+                                 'Full-stack Developer',
                                  'MERN-Stack Developer.',
                                  'React Developer.',
                                  'Web Instructor'
